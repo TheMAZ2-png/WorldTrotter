eba01b6cac0e386619d62c63c44c18bdcd40572a
Christopher Mazurek <csm012004@outlook.com>
2025-09-23 16:17:13 -0400

    Tester

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -5,4 +5,13 @@
 //  Created by Chris Mazurek on 9/11/25.
 //
 
-import Foundation
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        print("MapViewController loaded its view.")
+    }
+}
